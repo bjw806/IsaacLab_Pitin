9dfbc6bf15be314f8877f0a3f88b9f9406bc0514
pitin <pitin@pitin-ev.com>
2024-11-21 11:02:03 +0900

    .

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/skrl_test/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/skrl_test/__init__.py
@@ -18,5 +18,17 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.manage_agv_env:AGVEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_feature_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AGVPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Isaac-Pin-Managed",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.manage_pin_env:AGVEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_feature_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AGVPPORunnerCfg",
     },
 )
